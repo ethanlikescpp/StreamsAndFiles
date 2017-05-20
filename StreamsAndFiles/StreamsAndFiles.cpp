6ceb9e4d6dfe653ee2ae5c8af14cccc53d676e60
Ethan Chao <ethanlikespi@gmail.com>
2017-05-19 23:42:01 -0700

    Played a little with cin

--- StreamsAndFiles/StreamsAndFiles.cpp
+++ StreamsAndFiles/StreamsAndFiles.cpp
@@ -2,10 +2,23 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
-int main()
-{
+int main() {
+
+	double unitPrice;
+	int quantity;
+
+	cout << "What is the unit price? $";
+	cin >> unitPrice;
+
+	cout << endl << "How many do you want? ";
+	cin >> quantity;
+
+	cout << endl << "Total cost is: $" << unitPrice * quantity << endl;
+
     return 0;
 }
 
